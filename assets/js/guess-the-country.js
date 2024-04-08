260fcb271ca78ffb148f5870e691b18b2c286e66
Artem Bryzh <aab@AMBM2.local>
2024-04-08 15:53:21 +0100

    Add function to shuffle an array

--- assets/js/guess-the-country.js
+++ assets/js/guess-the-country.js
@@ -5,3 +5,17 @@ import flagsDataArray from './flags-data.js';
 function getRandomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+// Function to shuffle an array
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+
+        // Generating a random index j within the range [0, i]
+        const j = Math.floor(Math.random() * (i + 1));
+
+        // Swapping elements in the array to shuffle it
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
