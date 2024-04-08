73e02cdc91f205cddac75734c46a4c9e3dad3e5c
Artem Bryzh <aab@AMBM2.local>
2024-04-08 15:44:55 +0100

    Add function to get a random element from an array

--- assets/js/guess-the-country.js
+++ assets/js/guess-the-country.js
@@ -1,7 +1,7 @@
 // Importing the array of flag data from the flags-data.js file
 import flagsDataArray from './flags-data.js';
 
-// Filtered arrays by difficulty
-let easyFlags = flagsDataArray.filter(flag => flag.difficulty === "Easy");
-let mediumFlags = flagsDataArray.filter(flag => flag.difficulty === "Medium");
-let hardFlags = flagsDataArray.filter(flag => flag.difficulty === "Hard");
+// Function to get a random element from an array
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
